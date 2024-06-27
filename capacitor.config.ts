9f799537d81b0c22cf3ca5cb35ee1bc20e0815a1
Gabriel de Souza <gabriel.dsouzapro@gmail.com>
2024-06-27 14:25:44 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'duel-counter',
+  webDir: 'www'
+};
+
+export default config;
