6a175dfd851fc6ea1b6c1f2385d8edc3a3e1b2eb
sougabriel <gabriel.dsouzapro@gmail.com>
2024-08-11 21:51:16 -0300

    feat: create life calculator service

---
+++ src/app/calculator.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { Profile } from './interfaces/profile';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CalculatorService {
+  constructor() {}
+
+  sumLife(life: number, profile: Profile) {
+    return (profile.life! += life);
+  }
+
+  subLife(life: number, profile: Profile) {
+    profile.life! -= life;
+    if (profile.life! < 0) {
+      profile.life! = 0;
+    }
+  }
+
+  halfLife(profile: Profile) {
+    return (profile.life! /= 2);
+  }
+
+  doubleLife(profile: Profile) {
+    return (profile.life! *= 2);
+  }
+
+  kill(profile: Profile) {
+    return profile.life! = 0;
+  }
+}
